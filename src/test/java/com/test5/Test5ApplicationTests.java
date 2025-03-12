e548ff15cd407da86c4f5255fe60c6c1d5165400
santhosh111222 <hsanthoshsuresh@gmail.com>
2025-03-12 23:25:33 +0530

    Update Test5ApplicationTests.java

--- src/test/java/com/test5/Test5ApplicationTests.java
+++ src/test/java/com/test5/Test5ApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test5ApplicationTests {
-
+	int x = 10;
+         boolean ispresent=true;
 	@Test
 	void contextLoads() {
 	}
