daa1cbd692945ab4b06903a1995478f776cfb708
santhosh111222 <hsanthoshsuresh@gmail.com>
2025-03-13 11:47:41 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -8,6 +8,7 @@ public class Test5Application {
         int  x = 99;
 	int y =100;
 	double z = 89.3;
+	char x = 'a';
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
