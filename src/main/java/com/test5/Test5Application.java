0cf16ce924e1f4299f66a114bc4f4aa6ec201f8b
DELL <hsanthosh144@gami..com>
2025-03-13 10:19:03 +0530

    First message

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
+       boolean isPresent= true;
+        double z = 78.8;         
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
